11ce7fd7404187aa31203eb8d42cda88491921af
VanessaCarvalho-VNS <vnscarvalh@gmail.com>
2022-02-19 17:43:32 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web Mvc App from C# Course.";
+            ViewData["Desenvolvedora"] = "Vanessa Carvalho";
 
             return View();
         }
